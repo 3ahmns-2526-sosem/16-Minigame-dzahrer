34930295390e5d9b70fb7c56097ef48edc6f2492
Dominik Barley <dominik.zahrer08@htl-salzburg.ac.at>
2026-06-24 14:54:44 +0200

    Add win condition and feedback

--- Assets/MyGame/Scripts/ClickerGame.cs
+++ Assets/MyGame/Scripts/ClickerGame.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 public class ClickerGame : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public GameObject winText;
 
     [SerializeField] private int score = 0;
+    [SerializeField] private bool gameWon = false;
 
     private void OnMouseDown()
     {
+        if (gameWon)
+            return;
+
         score++;
 
         scoreText.text = "Score: " + score;
+
+        if (score >= 10)
+        {
+            gameWon = true;
+            winText.SetActive(true);
+        }
     }
 }
